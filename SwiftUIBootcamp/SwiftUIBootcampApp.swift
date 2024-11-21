da82076dd4b9d7a0ffaedd13ac7fd61457c5b47d
GarifullinII <ildaridrisovich@gmail.com>
2024-11-21 12:42:09 +0400

    initial commit

---
+++ SwiftUIBootcamp/SwiftUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampApp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Ildar Garifullin on 21.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
